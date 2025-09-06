b3d68ca648f6432bd73407daa5c138eda25fc70e
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-09-06 19:00:26 +0530

    commit

---
+++ 6-java/learning/b2.java
@@ -0,0 +1,15 @@
+public class b2{
+  public static void main(String arg[]){
+    //solid Rohambas 
+    int n=5;
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=n-i;j++){
+        System.out.print(" ");
+      }
+      for(int j=1;j<=n;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
---
+++ 6-java/learning/butter.java
@@ -0,0 +1,29 @@
+public class butter{
+  public static void main(String arg[]){
+    int n=4;
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=i;j++){
+        System.out.print("*");
+      }
+      for(int j=1;j<=2*(n-i);j++){
+        System.out.print(" ");
+      }
+      for(int j=1;j<=i;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    for(int i=4;i>=1;i--){
+      for(int j=1;j<=i;j++){
+        System.out.print("*");
+      }
+      for(int j=1;j<=2*(n-i);j++){
+        System.out.print(" ");
+      }
+      for(int j=1;j<=i;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
