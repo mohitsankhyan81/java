9deb5e6b8c4fac8329fd57b8e58e40fba83a35a8
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-09-11 21:30:18 +0530

    commit

---
+++ opps/object.java
@@ -0,0 +1,18 @@
+public class object{
+  class Rectangle{
+    int length;
+    int breadth;
+
+    int area(){
+      return length*breadth;
+    }
+  }
+  public static void main(String arg[]){
+    object obj=new object();
+    Rectangle r=obj.new Rectangle();
+
+    r.length=10;
+    r.breadth=10;
+    System.out.println("Area "+r.area());
+  }
+}
