5b36e6012c33ff6fec0c4a3d917f171b7c6e7154
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-09-16 21:23:05 +0530

    commit

--- 17-java/Question/towerofhonoi.java
+++ 17-java/Question/towerofhonoi.java
@@ -1,15 +1,15 @@
 public class towerofhonoi{
-  public static void honoitower(int n,String src,String helper,String dist){
+  public static void hnoioftower(int n,String src,String help,String des){
     if(n==1){
-      System.out.println("Tower Looklike " +n+" From "+src+" " +helper);
+      System.out.println("Tower honi "+n+" from "+src+" to "+help);
       return;
     }
-    honoitower(n-1, helper, src, dist);
-    System.out.println("Tower Looklike " +n+" From "+src+" " +helper);
-    honoitower(n-1, src, dist, helper);
+    hnoioftower(n-1, help, src, des);
+    System.out.println("Tower honi "+n+" from "+src+" to "+help);
+    hnoioftower(n-1, src, des, help);
   }
-  public static void main(String arg[]) {
-      int n=3;
-      honoitower(n, "A", "B", "C");
+  public static void main(String arg[]){
+    int n=3;
+    hnoioftower(n, "A", "B", "C");
   }
 }
