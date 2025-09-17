482d5a8c9bb6d326741935caa9eb2ff1b704c489
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-09-17 21:20:31 +0530

    commit

---
+++ 21-java/question/basic5.java
@@ -0,0 +1,16 @@
+class employ{
+  int id;
+  String name;
+}
+
+public class basic5 {
+  public static void main(String arg[]){
+    System.out.println("THis is the costom calss");
+    employ harray=new employ();
+    //setting attributes
+    harray.name="Mohit sankhyan";
+    harray.id=435;
+    System.out.println(harray.id);
+    System.out.println(harray.name);
+  }
+}
