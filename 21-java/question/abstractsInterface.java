9c95651dbda7f87652c22dd5e5a31eb41aaf434b
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-09-24 09:40:45 +0530

    commit

--- 21-java/question/abstractsInterface.java
+++ 21-java/question/abstractsInterface.java
@@ -1,5 +1,24 @@
+interface Bicycle{
+  void applybreak(int decrement);
+  void speedup(int increment);
+}
+
+class MohitCycle implements Bicycle{
+  void bioHorn(){
+    System.out.println("pee pee poo poo");
+  }
+  public void applybreak(int decrement){
+    System.out.println("break apply");
+  }
+  public void speedup(int increment){
+    System.out.println("Applying speedup");
+  }
+}
 public class abstractsInterface {
   public static void main(String arg[]){
-    
+    MohitCycle c=new MohitCycle();
+    //you can create properties in Interfaces
+    //you can not modify you properties in interfacees
+    c.applybreak(1);
   }
 }
