05d1904b34139f2eb33659e0a233778c25b698c2
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-10-03 16:08:02 +0530

    commit

---
+++ package/Instruction.java
@@ -0,0 +1,12 @@
+//int java package
+
+//first -> phle to cd se folder ke andar jana jaruri hai
+
+//second jo code likha hai use run karo jis se us ka execution file aa jaye gi
+// -> javac foldername/*.java
+
+//third jo run able file hai us ko run karo
+// -> javac filename.javac
+
+//run using 
+// -> javac filename
