f415c6ec3e92f72ae656fac6d5d3d9e4926850cb
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-10-28 10:23:54 +0530

    commit

---
+++ mergesort.java
@@ -0,0 +1,51 @@
+public class mergesort {
+
+  public static void mergeshort2(int arr[],int si,int mid,int ei){
+    int[] merger=new int[ei-si+1];
+    int idx1=si;
+    int idx2=mid+1;
+    int x=0;
+
+    while(idx1<=mid&&idx2<=ei){
+      if(arr[idx1]<=arr[idx2]){
+        merger[x++]=arr[idx1++];
+      }
+      else{
+        merger[x++]=arr[idx2++];
+      }
+    }
+
+    while(idx1<=mid){
+      merger[x++]=arr[idx1++];
+    }
+    
+    while(idx2<=ei){
+      merger[x++]=arr[idx2++];
+    }
+
+    for(int i=0,j=si;i<merger.length;i++,j++){
+      arr[j]=merger[i];
+    }
+  }
+
+  public static void mergesort1(int arr[],int si,int ei){
+    if(si>=ei){
+      return;
+    }
+    int mid=si+(ei-si)/2;
+
+    mergesort1(arr, si, mid);
+    mergesort1(arr, mid+1, ei);
+    mergeshort2(arr, si, mid, ei);
+  }
+  public static void main(String arg[]){
+    int[] arr={3,5,2,6,4,8,1};
+    int n=arr.length;
+
+    mergesort1(arr,0,n-1);
+
+    for(int i=0;i<n;i++){
+      System.out.print(arr[i]+" ");
+    }
+  }
+}
